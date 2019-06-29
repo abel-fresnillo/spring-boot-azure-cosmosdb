40f2e2b78909691bab85101cc8f157252fd1ffd8
Abel Fresnillo <me@abel.dev>
2019-06-29 19:03:24 -0400

    first commit

---
+++ src/main/java/dev/abel/springbootazurecosmosdb/SpringBootAzureCosmosdbApplication.java
@@ -0,0 +1,13 @@
+package dev.abel.springbootazurecosmosdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureCosmosdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureCosmosdbApplication.class, args);
+	}
+
+}
